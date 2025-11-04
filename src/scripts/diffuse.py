cbcb74ae05168d6e7c7c168923ef91f7255984cf
Viggo Gascou <contact@viggogascou.co>
2025-11-04 13:45:43 +0100

    feat: update diffuse script

--- src/scripts/diffuse.py
+++ src/scripts/diffuse.py
@@ -1,19 +1,27 @@
+import logging
+
+import matplotlib.pyplot as plt
+
 from rsp.editing import AnycostDirections
 from rsp.loading import load_model
+from rsp.log_utils import set_logging_level
 
-sd = load_model("pixel", device="cuda", h_space="after", num_inference_steps=10)
+set_logging_level(logging.INFO)
+
+sd = load_model("pixel", device="cuda", h_space="after", num_inference_steps=100)
 
 # h_space Is the sementic latent space defined as ["before", middle","after"] the
 # middle convolution in the U-net
 
+
 ad = AnycostDirections(
     sd,
-    etas=None,  # Noise schedule. None/0 for DDIM, 1 for DDPM. List if eta_t values is also supported
-    num_examples=100,  # Choose num_examples top(bottom), positive(negative) examples for a given attribute from the sampled images
+    etas=None,  # etas Noise schedule. None/0 for DDIM, 1 for DDPM. or list of eta_t's
+    num_examples=100,
     idx_size=10000,  # Size of index. The number of images sampled in total
+    batch_size=32,
 )
 
-import matplotlib.pyplot as plt
 
 q_original = sd.sample(
     seed=42
