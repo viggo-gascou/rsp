bb85d5a98962ff39e10a30f7b68d8afa73c6bd20
Viggo Gascou <contact@viggogascou.co>
2025-12-14 18:28:45 +0100

    fixes

--- src/scripts/au_differences.py
+++ src/scripts/au_differences.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas
+import pandas as pd
 import seaborn as sns
 
 from rsp.constants import AU_SUBSET, FIGURES_DIR, RESULTS_DIR, SUPPORTED_AUS
@@ -9,7 +9,7 @@ AU_EDITING_DIR = RESULTS_DIR / "au_editing/"
 
 # get the bar plot data for the original images (its in the) au_predictions.csv file
 original_csv_path = AU_EDITING_DIR / "original_au_predictions.csv"
-original_df = pandas.read_csv(original_csv_path)
+original_df = pd.read_csv(original_csv_path)
 
 # save the plot of the csv (its just the au values for
 # each au in AU_SUBSET) so a bar plot
@@ -30,7 +30,7 @@ ax.set_xlabel("Action Units (AUs)")
 ax.set_xticks(range(len(SUPPORTED_AUS)))
 ax.set_xticklabels(SUPPORTED_AUS, rotation=45)
 plt.tight_layout()
-plt.savefig(FIGURES_DIR / "original_au_predictions.png", bbox_inches="tight", dpi=250)
+plt.savefig(FIGURES_DIR / "original_au_predictions.pdf", bbox_inches="tight", dpi=250)
 plt.close(fig)
 
 au_folders = [
@@ -39,14 +39,17 @@ au_folders = [
     if path.is_dir() and not path.name.startswith(".")
 ]
 
-for au in au_folders:
-    for result in au.glob("**/*.csv"):
-        if result.parent.name != "simple":
+for au_folder in au_folders:
+    for result in au_folder.glob("**/*.csv"):
+        mode = result.parent.name
+        if au_folder.name not in AU_SUBSET:
             continue
-        if au.name not in AU_SUBSET:
+        scale = result.stem.split("_")[-1].removeprefix("scale")
+
+        if scale != "1.0":
             continue
 
-        au_df = pandas.read_csv(result)
+        au_df = pd.read_csv(result)
         fig, ax = plt.subplots(figsize=(10, 6))
         sns.barplot(
             x=SUPPORTED_AUS,
@@ -56,20 +59,21 @@ for au in au_folders:
             hue=SUPPORTED_AUS,
             legend=False,
         )
-        ax.set_title(f"{au.name} Edited Images - Average AU Scores")
+        ax.set_title(f"{au_folder.name} {mode} Edited Images - Average AU Scores")
         ax.set_ylabel("Difference in Average AU Score")
         ax.set_xlabel("Action Units (AUs)")
         ax.set_xticks(range(len(SUPPORTED_AUS)))
         ax.set_xticklabels(SUPPORTED_AUS, rotation=45)
         plt.tight_layout()
         plt.savefig(
-            FIGURES_DIR / f"{au.name}_au_predictions.png", bbox_inches="tight", dpi=250
+            FIGURES_DIR / f"{au_folder.name}_au_predictions_{mode}.pdf",
+            bbox_inches="tight",
+            dpi=250,
         )
         plt.close()
         # now plot the difference from original
         diff = au_df[SUPPORTED_AUS].mean() - original_df[SUPPORTED_AUS].mean()
         fig, ax = plt.subplots(figsize=(10, 6))
-        # red if negative green if positive
         sns.barplot(
             x=SUPPORTED_AUS,
             y=diff,
@@ -78,18 +82,19 @@ for au in au_folders:
             hue=SUPPORTED_AUS,
             legend=False,
         )
-        # ax.patches[SUPPORTED_AUS.index(au.name)].set_linewidth(3)
-        ax.patches[SUPPORTED_AUS.index(au.name)].set_color("darkorange")
-        ax.set_title(f"{au.name} Edited Images - Difference in Average AU Scores")
+        # ax.patches[SUPPORTED_AUS.index(au_folder.name)].set_linewidth(3)
+        ax.patches[SUPPORTED_AUS.index(au_folder.name)].set_color("darkorange")
+        ax.set_title(
+            f"{au_folder.name} {mode} Edited Images - Difference in Average AU Scores"
+        )
         ax.set_ylabel("Difference in Average AU Score")
         ax.set_xlabel("Action Units (AUs)")
         ax.set_xticks(range(len(SUPPORTED_AUS)))
-        # make tick for this au BOLD
         ax.set_xticklabels(SUPPORTED_AUS, rotation=45)
-        ax.get_xticklabels()[SUPPORTED_AUS.index(au.name)].set_weight("bold")
+        ax.get_xticklabels()[SUPPORTED_AUS.index(au_folder.name)].set_weight("bold")
         plt.tight_layout()
         plt.savefig(
-            FIGURES_DIR / f"{au.name}_au_predictions_difference.png",
+            FIGURES_DIR / f"{au_folder.name}_au_predictions_difference_{mode}.pdf",
             bbox_inches="tight",
             dpi=250,
         )
